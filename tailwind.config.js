aea8538eb1793f51e32632475089b88d2211405f
Lucas Weidas <lucasweidas@gmail.com>
2023-04-05 19:25:24 -0300

    Add starter styles configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        red: {
+          450: 'hsl(0, 100%, 67%)',
+        },
+        purple: {
+          450: 'hsl(259, 100%, 65%)',
+        },
+        neutral: {
+          150: 'hsl(0, 0%, 94%)',
+          250: 'hsl(0, 0%, 86%)',
+          550: 'hsl(0, 1%, 44%)',
+          900: 'hsl(0, 0%, 8%)',
+        },
+      },
+    },
   },
   plugins: [],
 };
